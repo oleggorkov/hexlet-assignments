4f5fd571460c7195621adbfc1e17e62bdcc0f5c6
Oleg <44262105+oleggorkov@users.noreply.github.com>
2023-10-23 21:51:58 +0300

    Update stack_test.rb

--- ruby-basics-ru/testing/test/stack_test.rb
+++ ruby-basics-ru/testing/test/stack_test.rb
@@ -4,9 +4,40 @@ require_relative 'test_helper'
 require_relative '../lib/stack'
 
 class StackTest < Minitest::Test
-  # BEGIN
-  
-  # END
+
+  def setup
+    @stack = Stack.new
+  end
+
+  def teardown; end
+
+  def test_push_element do
+    @stack.push(1)
+    assert_equal @stack.items, [1], "Ошибка: Некорректная работа метода push()"
+  end
+
+  def test_pop_element do
+    @stack.push(1)
+    @stack.push(2)
+    assert_equal @stack.pop, 2, "Ошибка: Некорректная работа метода pop()"
+    assert_equal @stack.items, [1], "Ошибка: Некорректная работа метода pop()"
+  end
+
+  def test_clear do
+    @stack.push(1)
+    @stack.push(2)
+    @stack.clear
+    assert_equal @stack.items, [], "Ошибка: Некорректная работа метода clear()"
+  end
+
+  def test_is_empty do
+    assert @stack.is_empty?, "Ошибка: Некорректная работа метода is_empty?"
+    @stack.push(1)
+    assert_not @stack.is_empty?, "Ошибка: Некорректная работа метода is_empty?"
+    @stack.pop
+    assert @stack.is_empty?, "Ошибка: Некорректная работа метода is_empty?"
+  end
+
 end
 
 test_methods = StackTest.new({}).methods.select { |method| method.start_with? 'test_' }
